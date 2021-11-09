4d2a4aa24a7827d274e83ecc666d6d70074a49f8
Andres Nunez <camilonunezz9812@gmail.com>
2021-11-08 22:28:54 -0500

    rest framework added

--- ride/circles/views.py
+++ ride/circles/views.py
@@ -7,5 +7,10 @@ def list_circles(request):
     public_circle = Circle.objects.filter(is_public=True)
     data = []
     for circle in public_circle:
-        print("hola")
-    return HttpResponse("Hola")
+        data.append({
+            'name': circle.name,
+            'slug_name': circle.slug_name,
+            'verified': circle.verified,
+        })
+    return JsonResponse(data, safe=False)
+ 
